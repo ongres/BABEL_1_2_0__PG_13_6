cff5163a7a7d132b9a38a37ccdb1dc2d974e98d8
Emanuel Calvo <3manuek@gmail.com>
2022-04-29 13:31:39 +0200

    Added BABEL 1.2 extensions
    Signed-off-by: Emanuel Calvo <3manuek@gmail.com>

---
+++ contrib/babelfishpg_tsql/src/hooks.h
@@ -0,0 +1,16 @@
+#ifndef PLTSQL_HOOKS_H
+#define PLTSQL_HOOKS_H
+#include "postgres.h"
+#include "catalog/catalog.h"
+#include "parser/analyze.h"
+
+extern IsExtendedCatalogHookType PrevIsExtendedCatalogHook;
+
+extern void InstallExtendedHooks(void);
+extern void UninstallExtendedHooks(void);
+
+extern bool output_update_transformation;
+extern char* extract_identifier(const char *start);
+
+extern char *update_delete_target_alias;
+#endif
